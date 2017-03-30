5a84d97ff76e325b214f52c0589c79d2420bc3bc
Nianyi Wang <WangNianyi2001@icloud.com>
2017-03-30 12:08:48 +0800

    Index UI

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 {
-	App({
-		onLaunch: function() {
-			// 调用API从本地缓存中获取数据
-			let logs = wx.getStorageSync('logs') || [];
-			// unshift() 有副作用，也不语义化
-			wx.setStorageSync('logs', [Date.now()].concat(logs));
-		},
-		getUserInfo: function(cb) {
-			if(this.globalData.userInfo) {
-				if(typeof cb == "function") {
-					cb(this.globalData.userInfo);
-				}
-			} else {
-				// 调用登录接口
-				wx.login({
-					success: () => wx.getUserInfo({
-						success: res => {
-							this.globalData.userInfo = res.userInfo;
-							if(typeof cb == "function") {
-								// 无需闭包，箭头函数 this 指向运行时上下文
-								cb(this.globalData.userInfo);
-							}
-						}
-					})
-				});
-			}
-		},
-		globalData: {
-			userInfo: null
-		}
-	});
+    let HandleUserInfo = (cb, userInfo) => {
+        if(typeof cb == 'function')
+            cb(userInfo);
+    };
+    App({
+        getUserInfo(cb) {
+            this.globalData.userInfo ?
+                HandleUserInfo(cb, this.globalData.userInfo) :
+                wx.login({
+                    success: () => wx.getUserInfo({
+                        success: res => HandleUserInfo(cb, this.globalData.userInfo = res.userInfo)
+                    })
+                });
+        },
+        globalData: {
+            userInfo:null
+        }
+    })
 }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,24 @@
-// pages/index/index.js
+var app = getApp()
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
   },
-  onReady:function(){
-    // 页面渲染完成
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   }
 })
