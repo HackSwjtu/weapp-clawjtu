eb386ef35553032d6a80ef7d265b26802149319f
Nianyi Wang <WangNianyi2001@icloud.com>
2017-03-30 20:42:45 +0800

    New Feature: Love

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,30 +1,33 @@
 {
   let app = getApp();
+  let posts = [
+    {
+      title: "挖掘机技术哪家强",
+      descriptions: [
+        {
+          type: "来源",
+          content: "蓝翔"
+        }
+      ],
+      preview: "挖掘机技术哪家强？山东技校找蓝翔！",
+      loved: "loved"
+    },
+    {
+      title: "震惊！99% 的人都不知道……",
+      descriptions: [
+        {
+          type: "招聘",
+          content: "UC 震惊部"
+        }
+      ],
+      preview: "经实验检测，酸雨的主要成分是脱碳甲醛……",
+      loved: "unloved"
+    }
+  ];
   Page({
     data: {
       userInfo: {},
-      posts: [
-        {
-          title: "挖掘机技术哪家强",
-          descriptions: [
-            {
-              type: "答案",
-              content: "蓝翔"
-            }
-          ],
-          preview: "挖掘机技术哪家强？山东技校找蓝翔！"
-        },
-        {
-          title: "震惊！99% 的人都不知道……",
-          descriptions: [
-            {
-              type: "招聘",
-              content: "UC 震惊部"
-            }
-          ],
-          preview: "经实验检测，酸雨的主要成分是脱碳甲醛……"
-        },
-      ]
+      posts: posts
     },
     onLoad() {
       app.getUserInfo(userInfo => this.setData({
