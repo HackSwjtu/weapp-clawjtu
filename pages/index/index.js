e9aedcb349f33bcf90f810afeb744b0e7ec51ae9
Desgard_Duan <desgard_duan@foxmail.com>
2017-03-30 23:25:18 +0800

    Fix some css page style

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,23 @@
 {
   let app = getApp();
-  let posts = [
-    {
+  let posts = [{
       title: "挖掘机技术哪家强",
-      descriptions: [
-        {
+      descriptions: [{
           type: "来源",
-          content: "蓝翔"
+          content: "蓝翔",
         }
       ],
-      preview: "挖掘机技术哪家强？山东技校找蓝翔！",
-      loved: "loved"
-    },
-    {
+      preview: "挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！挖掘机技术哪家强？山东技校找蓝翔！",
+      loved: "loved",
+    }, {
       title: "震惊！99% 的人都不知道……",
-      descriptions: [
-        {
+      descriptions: [{
           type: "招聘",
-          content: "UC 震惊部"
+          content: "UC 震惊部",
         }
       ],
       preview: "经实验检测，酸雨的主要成分是脱碳甲醛……",
-      loved: "unloved"
+      loved: "unloved",
     }
   ];
   Page({
