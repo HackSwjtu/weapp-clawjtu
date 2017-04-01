3d4fa1ecfeee51c1deea4ee74e22c198cc3e6821
Desgard_Duan <desgard_duan@foxmail.com>
2017-04-01 08:25:43 +0800

    Fix css style in index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,6 +12,23 @@
       app.getPosts(posts => this.setData(
         {posts: posts}
       ));
+    },
+    tapCompetition: function(event) {
+      // 竞赛模块暂不显示
+      wx.showModal({
+        title: '',
+        content: '学科竞赛列表正在建设中',
+        showCancel: false,
+        confirmText: '知道了',
+        success: function(res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
     }
+
   });
 }
